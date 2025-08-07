4dcaee137369ec6cde5e8f16b31ae0873ac3ac4e
abhishekkumar-snakescript <abhishekkumar@snakescript.com>
2025-08-07 15:52:31 +0530

    fixed indexing

--- resume_ai_backend/urls.py
+++ resume_ai_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("resume_api.urls")),
     path("admin/", admin.site.urls),
     path("api/auth/", include("users.urls")),
     path("api/resumes/", include("resume_api.urls")),
