70374e00f0baadfc37720176fa4a0eacdd46e0b0
abhishekkumar-snakescript <abhishekkumar@snakescript.com>
2025-08-08 11:23:48 +0530

    tried to fix the endpoint function

--- resume_api/urls.py
+++ resume_api/urls.py
@@ -4,10 +4,11 @@ from .views import process_resume, save_resume, get_latest_resume, get_resume_hi
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('process/', process_resume, name='process_resume'),
+    path('process/', views.process_resume, name='process_resume'),
     path('match/', views.match_analysis, name='match_analysis'),
-    path('save/', save_resume, name='save_resume'),
-    path('latest/', get_latest_resume, name='latest_resume'),
-    path('history/', get_resume_history, name='resume_history'),
+    path('resumes/process/', process_resume, name='process_resume'),  # existing
+    path('resumes/save/', save_resume, name='save_resume'),
+    path('resumes/latest/', get_latest_resume, name='latest_resume'),
+    path('resumes/history/', get_resume_history, name='resume_history'),
 ]
 
